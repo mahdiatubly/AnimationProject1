bcdf421cade4209f67da0dec5872f494299c7532
mahdiatubly <mahdiatubly@gmail.com>
2022-10-04 11:54:39 +0300

    The first draft of the game

---
+++ script1.js
@@ -0,0 +1,29 @@
+let challanges = ["What are the two types of Old English poetry?", "How many diphthongs are there in the English language?", "What is a female writer called?", "Supportive terms such as `oh` and `really` are called what?", "Where a speaker is completely blunt and direct (e.g. `Sit down!`) is called what?"]
+
+
+
+let i = 0;
+
+
+
+
+// let quest  = document.createElement('h2')
+// let body = document.querySelector('body')
+// quest.innerText = "What are the two types of Old English poetry?"
+// body.appendChild('quest')
+
+let questNum = 1
+
+let quest = document.getElementById('challange').innerText = challanges[0]
+let body = document.querySelector('body')
+let newlabel1 = document.querySelector('#choice1')
+
+function checkAns(letter){
+    if(letter === true){}
+}
+
+
+
+
+
+
