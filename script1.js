8b9d41f65a337c902ef6e2dc969951e2f4d23f1c
mahdiatubly <mahdiatubly@gmail.com>
2022-10-06 07:48:23 +0300

    Setting the winning page

--- script1.js
+++ script1.js
@@ -1,18 +1,7 @@
 let challanges = ["What are the two types of Old English poetry?", "How many diphthongs are there in the English language?", "What is a female writer called?", "Supportive terms such as `oh` and `really` are called what?", "Where a speaker is completely blunt and direct (e.g. `Sit down!`) is called what?"]
 
-
-
-let i = 0;
-
-
-
-
-// let quest  = document.createElement('h2')
-// let body = document.querySelector('body')
-// quest.innerText = "What are the two types of Old English poetry?"
-// body.appendChild('quest')
-
 let questNum = 1
+let counter = 0
 
 let quest = document.getElementById('challange')
 quest.innerText = challanges[0]
@@ -22,13 +11,12 @@ let newlabel2 = document.querySelector('#choice2')
 let newlabel3 = document.querySelector('#choice3')
 
 function checkAns(ans, newquest, newchoices1, newchoices2, newchoices3){
-   
         if(ans === true){
+            counter += 1
             quest.innerText = newquest
             newlabel1.innerText = newchoices1
             newlabel2.innerText = newchoices2
             newlabel3.innerText = newchoices3
-
         }
         else{
             quest.innerText = newquest
@@ -66,24 +54,21 @@ function action(){
         questNum += 1
     }
     else if(questNum === 6){
-        document.querySelector('#block1').style.backgroundColor = 'rgba(0,0,0,0)'
-        document.querySelector('#block2').style.backgroundColor = 'rgba(0,0,0,0)'
-        document.querySelector('#block3').style.backgroundColor = 'rgba(0,0,0,0)'
-        document.querySelector('#block4').style.backgroundColor = 'rgba(0,0,0,0)'
-        document.querySelector('#block5').style.backgroundColor = 'rgba(0,0,0,0)'
-        document.querySelector('#snowman').style.width = '70%'
-        document.querySelector('#snowman').style.height = '52.5vw'
-        let choices = document.getElementById('choices')
-        choices.remove()
-        let ans = document.getElementById('c3').checked
-        checkAns(ans, null, null, null, null)
-        
+        console.log(counter)
+        if(counter >= 3){
+            let link = document.querySelector('#link')
+            link.href = "win.html"
+        }
+        else{
+            let link = document.querySelector('#link')
+            link.href = "lose.html"
+        }   
     }
-    
 }
 
 function reset(){
     questNum = 1
+    counter = 0
     checkAns(true, challanges[0],
     'A. The heroic Germanic pre-Christian and the Christian',
     'B. Traditional and non-traditional',
@@ -94,9 +79,20 @@ function reset(){
     document.querySelector('#block3').style.backgroundColor = 'rgba(0,0,0,0)'
     document.querySelector('#block4').style.backgroundColor = 'rgba(0,0,0,0)'
     document.querySelector('#block5').style.backgroundColor = 'rgba(0,0,0,0)'
-    document.querySelector('#snowman').style.width = '35%'
-    document.querySelector('#snowman').style.height = '26.5vw'
+    document.querySelector('.snowman').style.width = '35%'
+    document.querySelector('.snowman').style.height = '26.5vw'
+    document.querySelector('#block3').style.margin = '0 0 0 0'
+    document.querySelector('#block3').innerText = null
 
+    document.querySelector('#block1').style.border = '.25vw solid'
+    document.querySelector('#block2').style.border = '.25vw solid'
+    document.querySelector('#block3').style.border = '.25vw solid'
+    document.querySelector('#block4').style.border = '.25vw solid'
+    document.querySelector('#block5').style.border = '.25vw solid'
+
+    document.querySelector('.snowman').removeAttribute('id')
+    let link = document.querySelector('#link')
+    link.href = '#'
 }
 
 document.getElementById('check').addEventListener('click',action);
